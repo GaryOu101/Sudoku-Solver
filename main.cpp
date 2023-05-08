01145c4bbae3150fe8ef0e8e9accebabb538e8d6
mattrogrady <mattrogrady@gmail.com>
2023-05-08 01:26:49 -0700

    Changed operators.h and edited main.cpp

--- main.cpp
+++ main.cpp
@@ -3,6 +3,7 @@
 #include <string>
 #include <vector>
 #include "algorithms.h"
+#include "operators.h"
 
 using namespace std;
 
--- operators.h
+++ operators.h
@@ -1,107 +1,100 @@
-#ifndef OPERATORS_H_
-#define OPERATORS_H_
+#ifndef OPERATORS_H
+#define OPERATORS_H
 
 #include <iostream>
 #include <vector>
 
 using namespace std;
 
-typedef vector<vector<int>> board; //3x3 board with 2d vectors
-
-
-board swapNum(int zero_r, int zero_c, int num2_r, int num2_c, board puzzle){ //swaps pos of zero and number
-    puzzle[zero_r][zero_c] = puzzle[num2_r][num2_c];
-    puzzle[num2_r][num2_c] = 0;
-    return puzzle; 
+// Swaps 0 with another square
+vector < vector<int> > swapPos(int zero_r, int zero_c, int num2_r, int num2_c, vector < vector<int> > puzzle) {
+    puzzle.at(zero_r).at(zero_c) = puzzle.at(num2_r).at(num2_c);
+    puzzle.at(num2_r).at(num2_c) = 0;
+    return puzzle;
 }
 
-board moveDown(board puzzle){
-    int zero_row = 0;
-    int zero_col = 0;
-    
-    //locate where the 0 is on the board
-    for(int i = 0; i < 3; i++){
-        if(puzzle[0][i] == 0){
-                return puzzle; //do nothing bc shifting down does nothing to 0 on top row
-        }
-    }
-    // 0 is not in top row
+// Swaps 0 with space below it
+vector < vector<int> > moveDown(vector < vector<int> > puzzle) {
+    int zero_row = -1;
+    int zero_col = -1;
 
-    for(int i = 1; i < 3; i++){ //now check if middle or bottom row
-        for(int j = 0; j < 3; j++){
-            if(puzzle[i][j] == 0){
+    for (unsigned i = 0; i < 2; ++i) {
+        for (unsigned j = 0; j < 3; ++j) {
+            if (puzzle.at(i).at(j) == 0) {
                 zero_row = i;
                 zero_col = j;
             }
-        } 
-    }
-    return swapNum(zero_row, zero_col, zero_row -1, zero_col, puzzle);
- }
-
-board moveUp(board puzzle){
-    int zero_row = 0;
-    int zero_col = 0;
-    
-
-    for(int i = 0; i < 3; i++){ //check if 0 is in the bottom row
-        if(puzzle[2][i] == 0){ 
-                return puzzle; // do nothing 
-        }
-    }
-    for(int i = 1; i < 2; i++){ //find where 0 is
-        for(int j = 0; j < 3; j++){
-            if(puzzle[i][j] == 0){
-                zero_row = i;
-                zero_col = j;
-            }
-        } 
-    }
-    return swapNum(zero_row, zero_col, zero_row + 1, zero_col, puzzle);
-}
-
-board moveLeft(board puzzle){
-    int zero_row = 0;
-    int zero_col = 0;
-    
-    for(int i = 0; i < 3; i++){ //check if on right
-        if(puzzle[i][2] == 0){
-                return puzzle; //do nothing bc 
         }
     }
 
-    for(int i = 1; i < 3; i++){ //now check if middle or bottom row
-        for(int j = 0; j < 3; j++){
-            if(puzzle[i][j] == 0){
+    // Zero not in valid column
+    if (zero_row == -1) {
+        return puzzle;
+    }
+    return swapPos(zero_row, zero_col, zero_row + 1, zero_col, puzzle);
+}
+
+// Swaps 0 with space above it
+vector < vector<int> > moveUp(vector < vector<int> > puzzle) {
+    int zero_row = -1;
+    int zero_col = -1;
+
+    for (unsigned i = 1; i < 3; ++i) {
+        for (unsigned j = 0; j < 3; ++j) {
+            if (puzzle.at(i).at(j) == 0) {
                 zero_row = i;
                 zero_col = j;
             }
-        } 
-    }
-    return swapNum(zero_row, zero_col, zero_row, zero_col + 1, puzzle);
-}
-
-
-board moveRight(board puzzle){
-    int zero_row = 0;
-    int zero_col = 0;
-    
-
-    for(int i = 0; i < 3; i++){
-        if(puzzle[i][2] == 0){
-                return puzzle; //do nothing 
         }
     }
 
-    for(int i = 1; i < 3; i++){ 
-        for(int j = 0; j < 3; j++){
-            if(puzzle[i][j] == 0){
+    // Zero not in valid column
+    if (zero_row == -1) {
+        return puzzle;
+    }
+    return swapPos(zero_row, zero_col, zero_row - 1, zero_col, puzzle);
+}
+
+// Swaps 0 with space right of it
+vector < vector<int> > moveRight(vector <vector<int> > puzzle) {
+    int zero_row = -1;
+    int zero_col = -1;
+
+    for (unsigned i = 0; i < 3; ++i) {
+        for (unsigned j = 0; j < 2; ++j) {
+            if (puzzle.at(i).at(j) == 0) {
                 zero_row = i;
                 zero_col = j;
             }
-        } 
+        }
     }
-    return swapNum(zero_row, zero_col, zero_row, zero_col - 1, puzzle);
+
+    // Zero not in valid column
+    if (zero_row == -1) {
+        return puzzle;
+    }
+    return swapPos(zero_row, zero_col, zero_row, zero_col + 1, puzzle);
 }
 
+// Swaps 0 with space left of it
+vector < vector<int> > moveLeft(vector < vector<int> > puzzle) {
+    int zero_row = -1;
+    int zero_col = -1;
+
+    for (unsigned i = 0; i < 3; ++i) {
+        for (unsigned j = 1; j < 3; ++j) {
+            if (puzzle.at(i).at(j) == 0) {
+                zero_row = i;
+                zero_col = j;
+            }
+        }
+    }
+
+    // Zero not in valid column
+    if (zero_row == -1) {
+        return puzzle;
+    }
+    return swapPos(zero_row, zero_col, zero_row, zero_col - 1, puzzle);
+}
 
 #endif 
